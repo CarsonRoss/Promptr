feb2b439bc2c65365040ee31ec01760c7fe20bf8
CarsonRoss <carsonr317@gmail.com>
2025-11-09 14:48:40 -0600

    Netlify

---
+++ backend/vendor/bundle/ruby/3.2.0/gems/stripe-17.0.0/lib/stripe/api_version.rb
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec
+# frozen_string_literal: true
+
+module Stripe
+  module ApiVersion
+    CURRENT = "2025-09-30.clover"
+    CURRENT_MAJOR = "clover"
+  end
+end
---
+++ backend/vendor/bundle/ruby/3.2.0/gems/stripe-17.0.0/lib/stripe/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Stripe
+  VERSION = "17.0.0"
+end
